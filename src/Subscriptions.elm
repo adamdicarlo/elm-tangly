da885741f0f68b48cc0528c9c6490c65a0d46eae
Adam DiCarlo <adam.dicarlo@gmail.com>
2017-09-10 17:39:33 -0700

    :hear_no_evil: Ignore mouse events over HUD

--- src/Subscriptions.elm
+++ src/Subscriptions.elm
@@ -5,11 +5,31 @@ import Window
 import Types exposing (Model, Msg(..))
 
 
+{-| width and height of HUD must correspond to CSS
+-}
+hudWidth =
+    640
+
+
+hudHeight =
+    90
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch
-        [ Mouse.downs MouseDown
-        , Mouse.moves MouseMove
-        , Mouse.ups MouseUp
-        , Window.resizes WindowSize
-        ]
+    let
+        -- We only want to receive mouse events that happen within the playfield (not within
+        -- the HUD)
+        mouseEvent : (Mouse.Position -> Msg) -> Mouse.Position -> Msg
+        mouseEvent message { x, y } =
+            if y >= (model.height - hudHeight) && x <= hudWidth then
+                NoOp
+            else
+                message { x = x, y = y }
+    in
+        Sub.batch
+            [ Mouse.downs <| mouseEvent MouseDown
+            , Mouse.moves <| mouseEvent MouseMove
+            , Mouse.ups <| mouseEvent MouseUp
+            , Window.resizes WindowSize
+            ]

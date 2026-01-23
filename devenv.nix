5c0284eb104a6902cc92eede99f3dcd465447e31
Adam DiCarlo <adam@bikko.org>
2026-01-22 18:42:42 -0800

    Convert to devenv + bun

---
+++ devenv.nix
@@ -0,0 +1,83 @@
+{pkgs, ...}: {
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.bun
+    pkgs.git
+    pkgs.nodejs_24
+  ];
+
+  # https://devenv.sh/languages/
+  languages.elm = {
+    enable = true;
+    lsp.enable = true;
+  };
+  languages.javascript = {
+    lsp.enable = true;
+    bun = {
+      enable = true;
+      install.enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+
+  # https://devenv.sh/services/
+
+  # https://devenv.sh/scripts/
+  scripts.build = {
+    exec = ''
+      bunx parcel build src/index.html
+    '';
+    description = "Build the project for production";
+  };
+  scripts.start = {
+    exec = ''
+      bunx parcel serve src/index.html
+    '';
+    description = "Start the local development server";
+  };
+  scripts.tests = {
+    exec = ''
+      echo >&2 -e "\n# Running prettier"
+      bunx prettier -c .
+
+      echo >&2 -e "\n# Running elm-test-rs"
+      bunx elm-test-rs
+
+      echo >&2 -e "\n# Running elm-review"
+      bunx elm-review
+    '';
+    description = "Run tests";
+  };
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    echo -e '\nAll set! The following project commands are available in this shell:\n'
+
+    nix eval --impure --raw --expr '
+      let
+        pkgs = import <nixpkgs> {};
+        scripts = (import ./devenv.nix { inherit pkgs; }).scripts;
+      in
+      pkgs.lib.strings.concatLines
+        (builtins.map
+          (name: "- ''\${name}: " + scripts.''\${name}.description)
+          (builtins.attrNames scripts)
+        )
+    '
+  '';
+
+  # https://devenv.sh/tasks/
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks.elm-format.enable = true;
+  git-hooks.hooks.elm-test.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
